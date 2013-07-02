7120b4ea8d6e260a20c4fc92c7d403b2487dde2f
Ivan Danci <idanci@nebulent.com>
2013-07-03 00:10:03 +0300

    Redirect after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for (resource)
+    if resource.is_a?(User)
+      :root
+    end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
