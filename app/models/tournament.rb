e1e29e0342fe71c62c36277b62f9560626c9f9c5
Ivan Danci <idanci@nebulent.com>
2013-07-09 00:19:03 +0300

    Destroy tours with tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,5 @@
 class Tournament < ActiveRecord::Base
-  has_many :tours
+  has_many :tours, :dependent => :destroy
   has_and_belongs_to_many :pilots, :join_table => :competitions
 
   attr_accessible :name
